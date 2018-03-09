96b05a2e2b10356c43c6e4f46a4004ed46ada6f6
JUNYAO REN <jren@JUNYAOs-MBP.attlocal.net>
2018-03-09 03:50:43 -0600

    problem solved, test passed, destroy -> delete, but why?

--- course2/todolists_2/app/models/todo_list.rb
+++ course2/todolists_2/app/models/todo_list.rb
@@ -1,5 +1,5 @@
 class TodoList < ActiveRecord::Base
-  belongs_to :user, dependent: :delete
+  belongs_to :user#, dependent: :delete
   has_many :todo_items, dependent: :destroy
   default_scope{order :list_due_date}
 end
--- course2/todolists_2/app/models/user.rb
+++ course2/todolists_2/app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  has_one :profile, dependent: :destroy
-  has_many :todo_lists, dependent: :destroy
-  has_many :todo_items, through: :todo_lists, source: :todo_items,  dependent: :destroy
+  has_one :profile#, dependent: :destroy
+  has_many :todo_lists#, dependent: :destroy
+  has_many :todo_items, through: :todo_lists, source: :todo_items#,  dependent: :destroy
 
   validates :username, presence: true
 
